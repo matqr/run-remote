761d23c72693daa62f2227ccf3b0903e5e5b6fc1
Matias Quintana <matias.quintana.r@gmail.com>
2020-10-11 18:34:16 +0800

    full path for envs

--- copy_env.sh
+++ copy_env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # create copy of repo's environment
-cd $1/env/
+cd ~/Documents/$1/env/
 cp environment_macos.yaml environment_ubuntu.yaml
 
--- setup_environment.sh
+++ setup_environment.sh
@@ -3,7 +3,7 @@
 ## before running this script copy_env.sh should have been executed
 
 # get name of the curret environment
-name=$(grep -A3 'name:' env/environment_ubuntu.yaml | head -n 1 | awk '{print $2}')
+name=$(grep -A3 'name:' ~/$1/env/environment_ubuntu.yaml | head -n 1 | awk '{print $2}')
 
 # set it up for jupyter
 python -m ipykernel install --user --name=name
